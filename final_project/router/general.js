326fded2f365b12b06dbc930043dec4b32ff574e
devanipuvvala20-ship-it <devanipuvvala20@gmail.com>
2026-08-29 14:20:48 -0400

    Complete Task 5 get books by title

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -65,9 +65,13 @@ public_users.get('/author/:author', function (req, res) {
 
 public_users.get('/title/:title', function (req, res) {
 
-  //Write your code here
+  const title = req.params.title;
 
-  return res.status(300).json({message: "Yet to be implemented"});
+  const booksByTitle = Object.values(books).filter(
+    book => book.title.toLowerCase() === title.toLowerCase()
+  );
+
+  return res.json(booksByTitle);
 
 });
 
